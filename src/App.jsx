e3e72be8dde94da1686fe795ba81da09d68f9f27
OksanaNova <o.nova0804@gmail.com>
2025-09-15 10:05:37 -0600

    first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import './App.css'
 import StripeContainer from './Stripe/StripeContainer';
+import BERRIES from './assets/berries.jpg'
 
 function App() {
 
+  const [showCheckoutForm, setShowCheckoutForm] = useState(false);
+
   return (
     <div>
-      <StripeContainer />
+      {showCheckoutForm ? <StripeContainer /> : 
+      <div>
+        <p>FRUIT SHOP</p>
+        <p>$9.99</p>
+        <img src={BERRIES} width="500px"/>
+        <div>
+          <button onClick={() => setShowCheckoutForm(true)}>Buy</button>
+        </div>
+      </div>}
     </div>
   )
 }
